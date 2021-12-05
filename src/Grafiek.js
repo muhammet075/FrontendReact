34d8b58b0a50b00bd80e0ff06a7648e8dd6310aa
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-12-05 03:23:38 +0100

    d3 toegvoegd

--- src/Grafiek.js
+++ src/Grafiek.js
@@ -1,10 +1,132 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import * as d3 from "d3";
 
 function Grafiek() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://countriesnow.space/api/v0.1/countries/population"
+    );
+
+    const items = await data.json();
+
+    const nieuwItems = items.data[185].populationCounts;
+
+    const dataNederland = nieuwItems;
+
+    let nieuwInwoner = [];
+
+    for (let i = 0; i < dataNederland.length; i++) {
+      //afronden naar 3 getallen
+      var afronden = dataNederland[i].value.toString().slice(0, -4);
+
+      //een komma toevoegen na de miljoen getal
+      var decimalen = afronden.slice(0, 2) + "." + afronden.slice(2);
+
+      //string omzetten naar int
+      var nieuwInnwoner = Number(decimalen);
+
+      //pushen naar nieuwe array
+      nieuwInwoner.push(nieuwInnwoner);
+    }
+
+    console.log(nieuwInwoner);
+
+    //groottes van de margin width en height definieren
+    const margin = { top: 80, bottom: 0, left: 150, right: 0 };
+    const width = 1000 - (margin.left - margin.right);
+    const height = 1000 - (margin.top - margin.bottom);
+
+    //element selecteren svg aanmaken
+    const svg = d3
+      .select("#grafiekNL")
+      .append("svg")
+      .data(dataNederland.sort((a, b) => d3.descending(a.value, b.value)))
+      .attr("width", width + (margin.left + margin.right))
+      .attr("height", height + (margin.top + margin.bottom));
+
+    // margin toevoege
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // x en y schalen
+    const xschaal = d3.scaleLinear().range([width - margin.left, margin.right]);
+
+    const yschaal = d3
+      .scaleBand()
+      .domain([13, 17])
+      .paddingInner(0.3)
+      .range([margin.bottom, height - margin.top]);
+
+    // axis toevegen en charts en labels
+    const xaxis = d3.axisTop().scale(xschaal);
+    const g_xaxis = g.append("g").attr("class", "axis");
+    const yaxis = d3.axisLeft().scale(yschaal);
+    const g_yaxis = g.append("g").attr("class", "axis");
+
+    update(dataNederland);
+
+    function update(new_data) {
+      //scales/domeinen updaten
+      xschaal.domain([d3.max(new_data, (d) => d.value), 13]);
+      yschaal.domain(new_data.map((d) => d.year));
+
+      //axis displayen
+      g_xaxis.transition().call(xaxis);
+      g_yaxis.transition().call(yaxis);
+
+      // de juiste jaartallen toevoegen aan de juiste inwoner aantal
+      const rect = g
+        .selectAll("rect")
+        .data(new_data, (d) => d.year)
+        .join(
+          // rect en x toevegen
+          (enter) => {
+            const rect_enter = enter.append("rect").attr("x", 0);
+            return rect_enter;
+          }
+        );
+
+      // oude en nieuwe elementen enteren
+      rect
+        .transition()
+        .attr("height", yschaal.bandwidth())
+        .attr("width", (d) => xschaal(d.value))
+        .attr("y", (d) => yschaal(d.year));
+      rect.select("title").text((d) => d.year);
+    }
+
+    // checkbox
+    d3.select("#FilterJaren").on("change", function () {
+      const checked = d3.select(this).property("checked");
+      if (checked === true) {
+        // inwoners onder 16.4 miljoen verdwijnen
+        const gefilterd = dataNederland.filter((d) => d.value > 16400000);
+
+        return update(gefilterd);
+      }
+      update(dataNederland);
+      //alle data terug updaten
+    });
+  };
+
   return (
     <div className='inhoud'>
       <h1>Grafiek</h1>
+      <article id='grafiekNL'>
+        <h2>Inwoners Nederland</h2>
+        <section class='filter'>
+          <label>
+            <input type='checkbox' id='FilterJaren' />
+            Laatste 10 jaar
+          </label>
+        </section>
+      </article>
     </div>
   );
 }
